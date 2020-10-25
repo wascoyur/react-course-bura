43bccb951be5165f9c268a23638dece5f76fb492
wascoyur <mytem20007@gmail.com>
2020-10-25 22:19:23 +0500

    stop JSX.mp4

--- src/index.js
+++ src/index.js
@@ -1,4 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-const el = <h1>Привет реакт!!</h1>
-ReactDOM.render(el, document.getElementById('root'));
+
+const TodoList = () => {
+  const list = ['Learn React', 'Build Awesome App']
+  return (
+    <ul>
+      <li>{list[1]}</li>
+      <li>{list[0]}</li>
+    </ul>
+  )
+}
+const AppHeader = () => {
+  return(
+    <h1>My Todo List</h1>
+  )
+}
+const SearchPanel = () => {
+  return (
+    <input placeholder = "search" />
+  )
+}
+const App = () => {
+  const login = <span>Login, please</span>
+  return (
+        <div>
+          <span>{ (new Date()).toString() }</span>
+          { login }
+          <AppHeader/>
+          <SearchPanel/>
+          <TodoList/>
+        </div>
+)
+};
+ReactDOM.render(<App/>, document.getElementById('root'));
