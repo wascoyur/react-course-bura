de56f9fa84f3a250a7bec81324152890885b221d
wascoyur <mytem20007@gmail.com>
2020-10-29 22:12:27 +0500

    feat: сделана важность

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -3,26 +3,55 @@ import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
 
+  state = {
+    done:  false,
+    important: false
+  }
+
+  onLabelClick(){
+      this.setState(({done}) =>{
+        return {
+          done:!done
+        }
+    })
+  }
+
+  onMarkImportant(){
+    this.setState(({important}) => {
+      return {important: !important}
+    })
+    console.log('state:', this.state)
+  }
   render(){
-    const { label, important = false } = this.props;
-    const style = {
-      color: important ? 'steelblue' : 'black',
-      fontWeight: important ? 'bold' : 'normal'
-    };
+    const { label } = this.props;
+    const { done, important } = this.state;
+
+    let classNames = 'todo-list-item';
+    if (this.state.done) {
+      classNames += ' done'
+    }
+    if (this.state.important) {
+      classNames += ' important'
+    }
     return(
-      <span className="todo-list-item">
+      <span className={classNames}>
         <span
           className="todo-list-item-label"
-          style={ style }>
+          onClick={ () => {
+            this.onLabelClick();
+          }}
+          >
           { label }
         </span>
 
         <button type="button"
+                onClick = {() =>  this.onMarkImportant()}
                 className="btn btn-outline-success btn-sm float-right">
           <i className="fa fa-exclamation" />
         </button>
 
         <button type="button"
+
                 className="btn btn-outline-danger btn-sm float-right">
           <i className="fa fa-trash-o" />
         </button>
@@ -30,4 +59,3 @@ export default class TodoListItem extends Component {
   )
   }
 }
-
