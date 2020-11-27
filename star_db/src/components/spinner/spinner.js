efc84fc1006bc4561b9efa8c4e3709241affd517
wascoyur <mytem20007@gmail.com>
2020-11-27 21:07:52 +0500

    Revert "stop: Роутинг практически не делал"
    This reverts commit dd905ee079101969ba3b9bfb3bf2ef2c1baa6909.

--- star_db/src/components/spinner/spinner.js
+++ star_db/src/components/spinner/spinner.js
@@ -4,8 +4,7 @@ import './spinner.css';
 export default class Spinner extends Component{
   render(){
     return(
-      <div className="loadingio-spinner-double-ring-50gp1v2u54a">
-        <div class="ldio-gwirqlm4ykp">
+      <div class="loadingio-spinner-double-ring-50gp1v2u54a"><div class="ldio-gwirqlm4ykp">
       <div></div>
       <div></div>
       <div><div></div></div>
